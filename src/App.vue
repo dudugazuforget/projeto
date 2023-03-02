0da395334100d67cbdcc45c87655bf314006b505
dudugazuforget <dudu99744@gmail.com>
2023-03-02 15:17:37 -0300

    up

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<script>
+export default {
+  data() {
+    return {
+      contador: 0
+    }
+  },
+  methods: {
+    incrementarContador() {
+      this.contador++
+    },
+    decrementarContador() {
+      if(this.contador > 0){
+        this.contador--
+      }
+    },
+    reset() {
+      this.contador = 0
+    }
+  },
+  mounted() {
+    console.log(`O valor inicial do contador é ${this.contador}.`)
+  }
+}
+</script>
+
+<template>
+  <button @click="incrementarContador">Incrementar</button>
+  <button @click="decrementarContador">Decrementar</button>
+  <button @click="reset">Resetar</button>
+ <div class="contador">  {{ contador }} </div>
+</template>
+
+<style scoped>
+button {
+  background-color: white;
+  color: aqua;
+  border: 1px solid black;
+  font-weight: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+}
+button:hover{
+  background-color: black;
+  color: blue;
+}
+.contador{
+  border: 1px solid black;
+  font-weight: 50px
+  ;
+}
+</style>
